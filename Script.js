47e69405c6c829f041694bccf499955a9240a15b
Rakesh Raikwar <152499500+Rakesh250305@users.noreply.github.com>
2025-06-26 14:14:14 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-function scrollLeft() {
-  const container = document.getElementById('servicesContainer');
-  container.scrollBy({ left: -300, behavior: 'smooth' });
-}
-
-function scrollRight() {
-  const container = document.getElementById('servicesContainer');
-  container.scrollBy({ left: 300, behavior: 'smooth' });
-}
